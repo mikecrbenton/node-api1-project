34d03d8310c511a9dc357b489121df0ad9a2fe76
mikecrbenton <mikecrbenton@hotmail.com>
2020-10-19 21:34:00 -0500

    all mvp functionality working - need to add additional conditionals

---
+++ database.js
@@ -0,0 +1,62 @@
+const shortid = require('shortid')
+
+let users = [
+   {
+      id: shortid.generate(), 
+      name: "Person Number 1", 
+      bio: "Bio Number 1",  
+    },
+    {
+      id: shortid.generate(), 
+      name: "Person Number 2", 
+      bio: "Bio Number 2",  
+    },
+    {
+      id: shortid.generate(), 
+      name: "Person Number 3", 
+      bio: "Bio Number 3",  
+    }
+]
+
+module.exports = {
+   getUsers,
+   getUsersById,
+   createUser,
+   updateUser,
+   deleteUser
+}
+
+
+// GET
+function getUsers() {
+   return users
+}
+// GET BY ID
+function getUsersById(id) {
+   return users.find( user => user.id === id )
+}
+
+// POST / CREATE USER
+function createUser(data) {
+   // create new user
+   const newUser = {
+      id: shortid.generate(),
+      ...data
+   }
+   // add to array
+   users.push(newUser)
+   return newUser
+}
+
+// PUT
+function updateUser(id, data) {
+   const index = users.findIndex( user => user.id === id)
+
+   users[index] = { ...users[index], ...data }
+   return users[index]
+}
+
+// DELETE
+function deleteUser(id) {
+   users = users.filter( user => user.id !== id )
+}
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const port = 5000
 const server = express()
+//DATABASE ( FAKE ) 
+const db = require("./database")
+
+// Needed in order for Express to parse out JSON bodies 
+server.use( express.json() )
 
 server.get('/', (req,res) => {
    res.send('Hello World')
@@ -8,4 +13,67 @@ server.get('/', (req,res) => {
 
 server.listen( port, () => {
    console.log(`listening on port ${port}`)
+})
+
+// BEGIN FUNCTIONS------------------------
+
+// GET
+server.get("/users", (req, res) => {
+   // simulating a real call to a database
+   const users = db.getUsers()
+   res.json(users)
+})
+
+// GET BY ID
+server.get('/users/:id', (req,res) => {
+   const id = req.params.id
+   const user = db.getUsersById(id)
+
+   if(user){
+      res.json(user)
+   }else {
+      res.status(404).json( { message: "temp message" } )
+   }
+})
+
+// POST
+server.post('/users', (req,res) => {
+   if( req.body.name && req.body.bio) {
+
+      try{ 
+         const newUser = db.createUser( { name: req.body.name, bio : req.body.bio} )
+         res.status(201).json(newUser) // 201 = resource created
+      }catch{
+         res.status(500).json( { errorMessage: "Error while saving to database" } )
+      }
+
+   }else{
+      res.status(400).json( { errorMessage: " Please provide name and bio"} )
+   }  
+})
+
+// PUT
+server.put('/users/:id', (req,res)=> {
+   const id = req.params.id
+   const user = db.getUsersById(id)
+
+   if(user) {
+      const updateduser = db.updateUser( id, { name: req.body.name } )
+      res.json(updateduser)
+   }else{
+      res.status(404).json( { message: "temp"} )
+   }
+})
+
+// DELETE
+server.delete('/users/:id', (req,res) => {
+   const id = req.params.id
+   const user = db.getUsersById(id)
+
+   if(user) {
+      db.deleteUser(id)
+      res.status(204).end() // successful empty response
+   }else{
+      res.status(404).json( {message: "User not found"} ) 
+   }
 })
